256c2b2cc55b038471e3873323fef86e3020d37d
Creldra <04D87g96>
2020-05-25 16:04:16 +0800

    Main Page is pretty much done, just need you guys to create your own pages.

---
+++ app/src/main/java/com/example/mad_team1_assignment/ChapterMenu.java
@@ -0,0 +1,14 @@
+package com.example.mad_team1_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChapterMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chapter_menu);
+    }
+}
--- app/src/main/java/com/example/mad_team1_assignment/MainActivity.java
+++ app/src/main/java/com/example/mad_team1_assignment/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.mad_team1_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
+    Button optionButton;
+    Button exitButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startbutton);
+        optionButton = findViewById(R.id.optionbutton);
+        exitButton = findViewById(R.id.exitbutton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goChapterPage = new Intent(MainActivity.this, ChapterMenu.class);
+                startActivity(goChapterPage);
+            }
+        });
+
+        optionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        exitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+                builder.setMessage("Do you really want to exit the game");
+                AlertDialog exitAlert = builder.create();
+                exitAlert.show();
+            }
+        });
     }
 }
