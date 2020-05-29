a6d3058e391531eea4bfde99847e52683605c516
Si Min Nicole Sim <s10196790@connect.np.edu.sg>
2020-05-30 03:34:27 +0800

    Finished Chapter 1 button + Back button on ChapterMenu

--- app/src/main/java/com/example/mad_team1_assignment/ChapterMenu.java
+++ app/src/main/java/com/example/mad_team1_assignment/ChapterMenu.java
@@ -2,13 +2,36 @@ package com.example.mad_team1_assignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChapterMenu extends AppCompatActivity {
+    Button chapterOne;
+    Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chapter_menu);
+        chapterOne = findViewById(R.id.chapterOneButton);
+        backButton = findViewById(R.id.backButton);
+
+
+        chapterOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             Intent chapterOne = new Intent(ChapterMenu.this,StoryPage.class);
+             startActivity(chapterOne);
+            }
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
+
 }
